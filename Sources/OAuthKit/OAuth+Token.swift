afcab553a202aae02a6a6b8594dd59c580213d45
Kevin McKee <kevinmmckee@gmail.com>
2025-06-02 21:04:29 -0700

    OpenID Connect is now working.

--- Sources/OAuthKit/OAuth+Token.swift
+++ Sources/OAuthKit/OAuth+Token.swift
@@ -14,18 +14,41 @@ extension OAuth {
     /// See: https://datatracker.ietf.org/doc/html/rfc6749#section-5.1
     public struct Token: Codable, Equatable, Sendable {
 
+        /// The access token string as issued by the authorization server.
         public let accessToken: String
+
+        /// If the access token will expire, then this can be used to obtain another access token.
         public let refreshToken: String?
+
+        /// If the access token expires, this is the duration of time the access token is granted for (in seconds).
         public let expiresIn: Int?
-        public let state: String?
+
+        /// If the scope the user granted is identical to the scope the app requested, this parameter is optional.
+        /// If the granted scope is different from the requested scope, such as if the user modified the scope, then this parameter is required.
+        public let scope: String?
+
+        /// The type of token this is, typically just the string “Bearer”.
         public let type: String
 
-        public init(accessToken: String, refreshToken: String?, expiresIn: Int?, state: String?, type: String) {
+        /// The OpenID Connect issued by the authorization server.
+        /// This token is included if the authorization server supports OpenID connect and the scope included `openid`
+        public let openIDToken: String?
+
+        /// Common Initializer.
+        /// - Parameters:
+        ///   - accessToken: the access token string
+        ///   - refreshToken: the refresh token
+        ///   - expiresIn: the expiration time in secods
+        ///   - scope: the scope returned from the authorization server
+        ///   - type: the token type
+        ///   - openIDToken: the OpenID Connect token
+        public init(accessToken: String, refreshToken: String?, expiresIn: Int?, scope: String?, type: String, openIDToken: String? = nil) {
             self.accessToken = accessToken
             self.refreshToken = refreshToken
             self.expiresIn = expiresIn
-            self.state = state
+            self.scope = scope
             self.type = type
+            self.openIDToken = openIDToken
         }
 
         enum CodingKeys: String, CodingKey {
@@ -33,7 +56,8 @@ extension OAuth {
             case refreshToken = "refresh_token"
             case expiresIn = "expires_in"
             case type = "token_type"
-            case state
+            case scope
+            case openIDToken = "id_token"
         }
     }
 

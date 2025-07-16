6d8a2ecc79ff0101dc25c44089a02f129ea4d236
Code Fiesta <17772633+codefiesta@users.noreply.github.com>
2025-07-16 15:25:37 -0700

    URLProtocol Removal (#128)

--- Sources/OAuthKit/OAuthKit.docc/Resources/tutorial-code-files/url-protocol-step-4.swift
+++
@@ -1,7 +0,0 @@
-// Once the OAuth has been authorized
-// send a REST request to GitHub's API for a list of org repos
-// The Authorization header will automatically get included
-// in the request since it matches `api.github.com`
-let url: URL = .init(string: "https://api.github.com/users/codefiesta/repos")
-let request = URLRequest(url: url)
-let (data, response) = try await urlSession.data(for: request)

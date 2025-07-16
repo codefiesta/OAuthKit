b58415f01aa1b83e3746b2a17eebc701af5ad253
Kevin McKee <kevinmmckee@gmail.com>
2025-07-16 11:52:36 -0700

    Fixing the example user repo url

--- Sources/OAuthKit/OAuthKit.docc/Resources/tutorial-code-files/url-protocol-step-4.swift
+++ Sources/OAuthKit/OAuthKit.docc/Resources/tutorial-code-files/url-protocol-step-4.swift
@@ -2,6 +2,6 @@
 // send a REST request to GitHub's API for a list of org repos
 // The Authorization header will automatically get included
 // in the request since it matches `api.github.com`
-let url: URL = .init(string: "https://api.github.com/orgs/{ORG}/repos")
+let url: URL = .init(string: "https://api.github.com/users/codefiesta/repos")
 let request = URLRequest(url: url)
 let (data, response) = try await urlSession.data(for: request)

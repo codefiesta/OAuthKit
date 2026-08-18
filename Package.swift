85ab1c205708fd5db6a6dd9629c7ce44bf63c652
Kevin McKee <kevinmmckee@gmail.com>
2026-08-18 09:09:29 -0700

    WIP

--- Package.swift
+++ Package.swift
@@ -3,58 +3,39 @@
 
 import PackageDescription
 
-let supportedPlatforms: [SupportedPlatform] = [
-    .iOS(.v17),
-    .macOS(.v15),
-    .tvOS(.v18),
-    .visionOS(.v1),
-    .watchOS(.v10)
-]
-var packageDependencies: [Package.Dependency] = []
-var targetDependencies: [Target.Dependency] = []
-var linkerSettings: [LinkerSetting] = []
-let platforms: [Platform] = [.iOS, .macOS, .tvOS, .watchOS, .visionOS]
-
-#if os(Linux) || os(Android)
-// Android and Linux
-packageDependencies = [
-    .package(url: "https://github.com/apple/swift-crypto.git", from: .init(4, 5, 0))
-]
-targetDependencies = [
-    .product(name: "Crypto", package: "swift-crypto")
-]
-#else
-// Apple
-linkerSettings = [
-    .linkedFramework("CryptoKit",
-        .when(platforms: platforms)
-    ),
-    .linkedFramework("LocalAuthentication",
-        .when(platforms: platforms)
-    ),
-    .linkedFramework("Network",
-        .when(platforms: platforms)
-    ),
-    .linkedFramework("Security",
-        .when(platforms: platforms)
-    ),
-]
-#endif
-
 let package = Package(
     name: "OAuthKit",
-    platforms: supportedPlatforms,
+    platforms: [
+        .iOS(.v17),
+        .macOS(.v15),
+        .tvOS(.v18),
+        .visionOS(.v1),
+        .watchOS(.v10)
+    ],
     products: [
         .library(
             name: "OAuthKit",
             targets: ["OAuthKit"])
     ],
-    dependencies: packageDependencies,
+    dependencies: [
+        // Android / Linux Dependencies
+        .package(url: "https://github.com/apple/swift-crypto.git", from: .init(4, 5, 0))
+    ],
     targets: [
         .target(
             name: "OAuthKit",
-            dependencies: targetDependencies,
-            linkerSettings: linkerSettings
+            dependencies: [
+                .product(name: "Crypto",
+                         package: "swift-crypto",
+                         condition:
+                        .when(platforms: [.android, .linux])
+                )
+            ],
+            linkerSettings: [
+                .linkedFramework("LocalAuthentication",
+                    .when(platforms: [.iOS])
+                ),
+            ]
         ),
         .testTarget(
             name: "OAuthKitTests",
